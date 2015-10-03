d79281696da2bd8c107ace02a8632cd5558e7fed
Ryan Stelly <ryan.stelly@live.com>
2015-10-03 13:34:03 -0500

    simple gulp file with clean and js minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var del = require('del');
 
-gulp.task('default', function() {
-	console.log('foo');
+var DEST = 'dist';
+
+gulp.task('scripts', ['clean'], function() {
+	gulp.src('scripts/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest(DEST));
 });
+
+gulp.task('clean', function() {
+	return del(DEST);
+});
+
+gulp.task('default', ['clean', 'scripts']);
